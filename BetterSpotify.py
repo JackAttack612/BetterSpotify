8d1d1c490a215d9928c35c56721e7b01c8f8f8fe
JackAttack612 <jetflyer1130@gmail.com>
2021-10-11 20:58:13 -0400

    Update

--- BetterSpotify.py
+++ BetterSpotify.py
@@ -1,21 +1,24 @@
+from playsound import playsound
+import os
+import time
+from os import listdir
+from os.path import isfile, join
 
-# importing vlc module
-import vlc
-  
-# importing pafy module
-import pafy
-  
-# url of the video
-url = "https://www.youtube.com/watch?v=il_t1WVLNxk&list=PLqM7alHXFySGqCvcwfqqMrteqWukz9ZoE"
-  
-# creating pafy object of the video
-video = pafy.new(url)
-  
-# getting stream at index 0
-best = video.streams[0]
-  
-# creating vlc media player object
-media = vlc.MediaPlayer(best.url)
-  
-# start playing video
-media.play()
+def main():
+    print()
+    print(song_list)
+    song = input("\nWhat song do you want to play: ")
+    playsound(location + r'\songs\\' + song + r'.mp3')
+    main()
+
+
+location = os.path.dirname(__file__)
+song_list = os.listdir(location + r'\songs\\')
+
+print("INSTRUCTIONS")
+print("The list of files below are the songs that are available")
+print("Type the file name without the .mp3 or .wav")
+print("If you want to add songs add mp3 or wav files to the songs directory")
+print("Note: Do not put any other file types besides .mp3 or .wav in the songs folder")
+
+main()
